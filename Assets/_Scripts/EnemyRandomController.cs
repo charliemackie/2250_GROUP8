5e0f6664d1658f8f0fdeb655260aa0296144ad48
Jonathan Richards <jrich87@uwo.ca>
2020-03-17 00:50:25 -0400

    Adding the enemy movement scripts and baking the enviroment so that movement looks good

---
+++ Assets/_Scripts/EnemyRandomController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyRandomController : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(waiter());
+    }
+
+    public void Move(){
+        int minimum = -20;
+        int maximum = 20;
+        float moveX = Random.Range(minimum, maximum);
+        float moveZ = Random.Range(minimum, maximum);
+
+        Vector3 newPosition = new Vector3(transform.position.x + moveX, transform.position.y, transform.position.z + moveZ);
+        agent.SetDestination(newPosition);
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        int wait_time = Random.Range(0, 10);
+        yield return new WaitForSeconds(wait_time);
+        Move();
+    }
+}
